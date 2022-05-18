558712c983174a5e7be7a790c7988d8765f122fc
pezantish <e.e.a.sadek@gmail.com>
2022-05-18 16:44:45 +0100

    All tasks I think, unless i missed a couple.

---
+++ W6D3/AM/index.js
@@ -0,0 +1,18 @@
+console.log("E");
+console.info("S");
+console.warn("UK");
+console.error("Gemini");
+
+const make = "Make";
+const model = "Model";
+
+console.log(`My favourite car is ${make} and the model is ${model}`)
+console.log("The next text is %c COLOURFUL!", "color: orange; font-family:fantasy; font-style: Bold; background-color: black; padding: 10px");
+
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay = totalMoney-totalLeftToPay;
+
+let literal = `The total bill is £${totalMoney} the remaining amount of money to be paid is $${totalLeftToPay}`
+
+typeof(totalMoney);
---
+++ W6D3/iterate.js
@@ -0,0 +1,42 @@
+`use strict`
+//1
+let A;
+for(A = 100; A<=200; A++){
+    console.log(A);
+}
+//2
+for(A = 100; A<=200; A++){
+    if (A%2 === 0){
+        console.log("-");
+    } else{
+        console.log("*");
+    }
+}
+//3
+for(A = 1; A<=10; A++){
+    for(let i=1; i<=10; i++){
+        console.log(A);
+    }
+}
+
+//5: Date() nicked from solution, was originally done as strings. 
+//Other than the data type, my own work.
+let day = new Date().getDay();
+switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log(`It's a weekday`);
+        break;
+    case 6:
+        console.log(`Saturday`);
+        break;
+    case 0:
+        console.log(`Sunday`);
+        break;
+    default:
+        console.log(`Invalid range`);
+        break;
+  }
