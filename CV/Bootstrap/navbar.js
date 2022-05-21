e832bf10e056bd655c4ec81707e6bee3acf92229
pezantish <e.e.a.sadek@gmail.com>
2022-05-22 00:44:34 +0100

    CV Homemade nav bar

---
+++ CV/Bootstrap/navbar.js
@@ -0,0 +1,10 @@
+//STOLEN FROM https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
+//NOT FUNCTIONAL, BUT UNDERSTOOD (I think)
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
